fa5133058f6ad074d4d016610fe80a056c08acf2
taquan <chittai1221@gmail.com>
2017-07-23 13:36:26 +0000

    add data

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,11 @@
 class ToppagesController < ApplicationController
+  
+  include ToppagesHelper
+  
   def index
+    @google_vision_api_key = ENV['GOOGLE_VISION_API_KEY']
   end
+  
+  
+  
 end
---
+++ app/helpers/toppages_helper.rb
@@ -0,0 +1,19 @@
+module ToppagesHelper
+  def getVisionAPIresult(data)
+    require 'net/http'
+    api_key= @google_vision_api_key
+    
+    uri = URI.parse "https://vision.googleapis.com/v1/images:annotate?key=#{api_key}"
+    
+    request = Net::HTTP::Post.new(uri.request_uri, initheader = {'Content-Type' => 'application/json'})
+    request.body = data
+    
+    response = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
+        http.request(request)
+    end
+    
+    p "response"
+    p response
+    
+  end
+end
